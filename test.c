fd1379174ff6679953c81dd8ebd1d868083db6e2
Timo Eichelmann <teichelm@c4a1c8.42berlin.de>
2024-05-29 19:58:29 +0200

    fuuuuuu

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: teichelm <teichelm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 15:29:12 by timo              #+#    #+#             */
-/*   Updated: 2024/05/28 17:46:16 by teichelm         ###   ########.fr       */
+/*   Updated: 2024/05/29 19:55:36 by teichelm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,6 +98,8 @@ char	**split_cmds(char *str, char ch)
 		if (str[c.i] == ch && ((c.qcount39 % 2 || c.qcount34 % 2) != 1))
 		{
 			result[c.count++] = ft_substr(str, c.j, c.i - c.j);
+			while(str[c.i + 1] == ch)
+				c.i++;
 			c.j = c.i + 1;
 		}
 		if (!str[c.i + 1])
@@ -162,7 +164,7 @@ char	*remover(char *str)
 	c.i = 0;
 	c.qcount34 = 0;
 	c.qcount39 = 0;
-	while (str[c.i])
+	while (str && str[c.i])
 	{
 		if (str[c.i] == 39 && c.qcount34 % 2 != 1)
 			c.qcount39++;
@@ -172,6 +174,8 @@ char	*remover(char *str)
 			str = cut_str(str, c.i, 1);
 		if (str[c.i] == 39 && c.qcount34 % 2 != 1)
 			str = cut_str(str, c.i, 1);
+		if (!str[c.i])
+			c.i--;
 		c.i++;
 	}
 	return (str);
@@ -224,31 +228,138 @@ int	realloc_redirect(char ***red)
 	return (i);
 }
 
-int	file_length(char *s)
+int	array_size(char **arr)
 {
-	
+	int	i;
+
+	i = 0;
+	while (arr && arr[i])
+		i++;
+	return(i);
 }
 
-void	parse_redirect(char *str, char **red, int i)
+void	cpy_array(char **dest, char **src)
+{
+	int		i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	free(src);
+	dest[i] = NULL;
+	return ;
+}
+
+char	*filename(char *str, int i)
+{
+	t_count	c;
+
+	c.i = i;
+	c.qcount34 = 0;
+	c.qcount39 = 0;
+	while (str[c.i])
+	{
+		if (str[c.i] == 39 && c.qcount34 % 2 != 1)
+			c.qcount39++;
+		if (str[c.i] == 34 && c.qcount39 % 2 != 1)
+			c.qcount34++;
+		if ((str[c.i] == ' ' || str[c.i] == '	' || str[c.i] == '<' || !str[c.i + 1]
+			|| str[c.i] == '>') && (c.qcount34 % 2 || c.qcount39 % 2) != 1)
+		{
+			if (!str[c.i + 1])
+				c.i += 1;
+			return (ft_substr(str, i, c.i - i));
+		}
+		c.i++;
+	}
+	return (ft_substr(str, i, c.i + 1));
+}
+
+char	redirect_type(char *str)
+{
+	if (str[0] == '>' && str[1] != '>')
+		return ('1');
+	if (str[0] == '>' && str[1] == '>')
+		return ('2');
+	if (str[0] == '<' && str[1] != '<')
+		return ('3');
+	if (str[0] == '<' && str[1] == '<')
+		return ('4');
+	return (0);
+}
+
+char	**parse_redirect(char *str, char **red, int i)
 {
 	char	c;
+	char	**temp;
+	char	ind;
+	int		size;
 
+	ind = redirect_type(str + i);
 	c = str[i];
-	while (str[i++] == c)
-	while(str[i++] == ' ')
-	if (!*red)
+	while (str[i] == c)
+		i++;
+	while(str[i] == ' ')
+		i++;
+	if (red)
 	{
-		*red = malloc(sizeof(char *) * 2);
+		temp = red;
+		red = malloc(sizeof(char *) * (array_size(red) + 2));
+		cpy_array(red, temp);
 	}
 	else
-		*red = malloc(sizeof(char *) * )
+	{
+		red = malloc(sizeof(char *) * 2);
+		red[0] = NULL;
+	}
+	size = array_size(red);
+	red[size + 1] = NULL;
+	red[size] = filename(str, i);
+	red[size] = paste_str(red[size], " ", 0, 1);
+	red[size] = paste_str(red[size], &ind, 0, 1);
+	return (red);
 }
 
-char	**redirect(char **str, char **red)
+char	*cut_redirect(char *str, int i, char ch)
+{
+	t_count	c;
+
+	c.i = i;
+	c.qcount34 = 0;
+	c.qcount39 = 0;
+	while (str[c.i] == ch)
+		c.i++;
+	while(str[c.i] == ' ')
+		c.i++;
+	while (str[c.i])
+	{
+		if (str[c.i] == 39 && c.qcount34 % 2 != 1)
+			c.qcount39++;
+		if (str[c.i] == 34 && c.qcount39 % 2 != 1)
+			c.qcount34++;
+		if ((str[c.i] == ' ' || str[c.i] == '	' || !str[c.i + 1]
+			||  str[c.i] == '<' ||  str[c.i] == '>') 
+			&& (c.qcount34 % 2 || c.qcount39 % 2) != 1)
+		{
+			if (!str[c.i + 1])
+				c.i += 1;
+			return(cut_str(str, i, c.i - i));
+		}
+		c.i++;
+	}
+	return (str);
+}
+
+char	**redirect(char **s, char **red)
 {
 	t_count c;
+	char	*str;
 
 	c.i = 0;
+	str = *s;
 	c.qcount34 = 0;
 	c.qcount39 = 0;
 	while (str[c.i])
@@ -260,27 +371,40 @@ char	**redirect(char **str, char **red)
 		if ((str[c.i] == '<' || str[c.i] == '>')
 			&& (c.qcount34 % 2 || c.qcount39 % 2) != 1)
 		{
-			parse_redirect(str, red, c.i);
+			red = parse_redirect(str, red, c.i);
+			str = cut_redirect(str, c.i, str[c.i]);
+			if (ft_strlen(str) < c.i)
+				c.i = ft_strlen(str);
 			c.i--;
 		}
 		c.i++;
 	}
+	*s = str;
+	return (red);
 }
 
 void	parser(char *cmd, t_cmd *res, char **env)
 {
+	int	i;
+
+	i = 0;
+	res->red = NULL;
+	res->red = redirect(&cmd, res->red);
+	while(res->red[i])
+	{
+		res->red[i] = remover(res->red[i]);
+		res->red[i] = expander(res->red[i], env, 5/*exchange for exit code*/);
+		i++;
+	}
+	i = 0;
 	res->cmd = split_cmds(cmd, ' ');
 	res->end = 1;
-	res->red = NULL;
-	int i = 0;
 	while(res->cmd[i])
 	{
-		res->cmd[i] = expander(res->cmd[i], env, 5);
-		res->red = redirect(&res->cmd[i], res->red);
+		res->cmd[i] = expander(res->cmd[i], env, 5/*exchange for exit code*/);
 		res->cmd[i] = remover(res->cmd[i]);
-		printf("%s ", res->cmd[i++]);
+		i++;
 	}
-	printf("\n");
 }
 
 t_cmd *creator(char *input, char **env)
@@ -307,12 +431,59 @@ t_cmd *creator(char *input, char **env)
 		parser(splitted[i], &cmd[i], env);
 		i++;
 	}
+	free(splitted);
 	cmd[i].end = 0;
 	return (cmd);
 }
 
+void	free_cmd(t_cmd *cmd)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (cmd[i].end)
+	{
+		j = 0;
+		while (cmd[i].cmd[j])
+		{
+			free(cmd[i].cmd[j]);
+			j++;
+		}
+		j = 0;
+		while (cmd[i].red[j])
+		{
+			free(cmd[i].red[j]);
+			j++;
+		}
+		i++;
+	}
+	free(cmd);
+}
+
 int main(int argc, char **argv, char **env)
 {
+	int	i = 0;
+	int	j = 0;
 	char *s = readline(">");
 	t_cmd *res = creator(s, env);
+	while (res[i].end)
+	{
+		while(res[i].cmd[j])
+		{
+			printf("%s ", res[i].cmd[j]);
+			j++;
+		}
+		printf("\n");
+		j = 0;
+		while (res[i].red[j])
+		{
+			printf("'%s' ", res[i].red[j]);
+			j++;
+		}
+		printf("\n");
+		j = 0;
+		i++;
+	}
+	free_cmd(res);
 }
--- testing.c
+++ testing.c
@@ -6,21 +6,34 @@
 /*   By: teichelm <teichelm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 12:29:28 by teichelm          #+#    #+#             */
-/*   Updated: 2024/05/28 16:37:41 by teichelm         ###   ########.fr       */
+/*   Updated: 2024/05/29 15:50:09 by teichelm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	change(char *tmp, char *s)
+{
+	tmp[3] = s[0];
+	return ;
+}
+
 
 int main (int argc, char **argv)
 {
-	char *c = argv[1];
-	int i =0;
-	while (c[i])
-	{
-		if (c[i] == '<' || '>')
-			printf("hello");
-		i++;
-	}
+	char *tmp = malloc(sizeof(char) * 6);
+	char *s = malloc(sizeof(char) * (2));
+	tmp[0] = 'h';
+	tmp[1] = 'e';
+	tmp[2] = 'l';
+	tmp[3] = 'l';
+	tmp[4] = 'o';
+	tmp[5] = 0;
+	s[0] = 'u';
+	s[1] = 0;
+	printf("%s\n", tmp);
+	change(tmp, s);
+	printf("%s\n", tmp);
+	free(s);
+	free(tmp);
 }
